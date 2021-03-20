1b81255d5385dec48ffa8983fcafb45685bc0384
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-20 11:44:18 +0300

    feat: resolv 2 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+	
+	if (Array.isArray(array)) {
+		let newarr = [];
+ for ( let name of array) { 
+
+ 	if (typeof(name) == "string") {
+ 		
+ 		newarr.push(name.trim().toUpperCase().charAt(0));
+ 	}
+ 	
+ }
+ newarr.sort();
+ return newarr.join("");
+	}
+	else {
+	return false;
+}
 };
