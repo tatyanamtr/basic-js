efbb8904299adc4142ff4f77921d7aeea00b6041
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-22 19:35:38 +0300

    feat: resolve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,21 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	if (typeof sampleActivity == "string") {
+		let nsa = Number(sampleActivity);
+		if (typeof nsa == "number" && nsa > 0  && nsa < MODERN_ACTIVITY) {
+			let ln = Math.log(MODERN_ACTIVITY/nsa);
+			return Math.ceil(ln/(0.693/HALF_LIFE_PERIOD));
+		}
+		else {
+			return false;
+		}
+	}
+
+	else {
+		return false;
+		
+	}	
+ 	
 };
