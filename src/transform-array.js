8286aeb5ce0eb865f673c9eab4052abe35d2758d
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-27 16:44:25 +0300

    Feat: resolve task Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function transform(arr) {
+	if (!Array.isArray(arr)) {
+		throw new Error;
+	}
+	if (arr.length == 0) {
+		return arr;
+	}
+	let res = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == "--double-next") {
+			if (arr[i+1] != undefined) {
+				res.push(arr[i+1]);
+			}
+			else continue;
+			
+		}
+		else if (arr[i] == "--double-prev") {
+			if (arr[i-1] != undefined && arr[i-2] != "--discard-next") {
+				res.push(arr[i-1]);
+			}
+			else continue;
+			
+			
+		}
+		else if (arr[i] == "--discard-next") {
+			i++;
+			
+		}
+		else if (arr[i] == "--discard-prev" && arr[i-2] != "--discard-next") {
+			res.pop();
+		}
+		else if (arr[i] == "--discard-prev" && arr[i-2] == "--discard-next") {
+			continue;
+		}
+		else res.push(arr[i]);
+	}
+	return res;
 };
