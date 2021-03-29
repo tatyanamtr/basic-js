b01a520f4e96463b46d57b9d3a8b41e1b2306418
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-29 19:14:07 +0300

    Feat: resolve Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let result = "";
+
+ 	let addition = options.addition;
+ 	if (addition === undefined) {
+ 		addition = "";
+ 	} 
+ 	else String(addition);
+
+ 	let additionSeparator = options.additionSeparator;
+ 	if (!additionSeparator) {
+ 		additionSeparator = "|";
+ 	}
+ 	else String(additionSeparator);
+
+ 	let separator = options.separator;
+ 	if (!separator) {
+ 		separator = "+";
+ 	}
+ 	let repeatTimes = options.repeatTimes;
+ 	let additionRepeatTimes = options.additionRepeatTimes;
+
+ 	if (!repeatTimes) {
+ 		return str+addition;
+ 	}
+ 	else if (!additionRepeatTimes){
+ 		let strSep = str + addition + separator;
+ 	result+=strSep.repeat(repeatTimes).slice(0, -separator.length);
+ 	return result;
+ 	}
+ 	else {
+ 		let addAddSep = addition+additionSeparator;
+ 		let strSep =str +addAddSep.repeat(additionRepeatTimes).slice(0, -additionSeparator.length);
+ 		result+=strSep+separator;
+ 		return result.repeat(repeatTimes).slice(0, -separator.length);
+ 	}
+
+ 	
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+  	if (!message || !key) {
+  		throw new Error;
+  	}
+
+   
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+  		throw new Error;
+  	}
   }
 }
 
